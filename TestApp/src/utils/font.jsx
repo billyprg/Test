3205f6c4623d3a45491b0c632546a94f713f05e2
Nabeel <109620622+billyprg@users.noreply.github.com>
2023-08-03 12:35:54 +0500

    setup

---
+++ TestApp/src/utils/font.jsx
@@ -0,0 +1,11 @@
+export const Font = {
+    PoppinsLight: 'Poppins-Light',
+    PoppinsRegular: 'Poppins-Regular',
+    PoppinsMedium: 'Poppins-Medium',
+    PoppinsSemiBold: 'Poppins-SemiBold',
+    PoppinsBold: 'Poppins-Bold',
+    PoppinsExtraBold: 'Poppins-ExtraBold',
+    PoppinsBlack: 'Poppins-Black',
+    
+  };
+  
